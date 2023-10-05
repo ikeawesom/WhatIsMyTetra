beba91fa1bbc93e455abd218804ae304b0df103a
Ike <ikelimjl@gmail.com>
2023-10-05 20:08:33 +0800

    util components

---
+++ src/components/utils/Container.tsx
@@ -0,0 +1,43 @@
+"use client";
+import React from "react";
+import { motion } from "framer-motion";
+import { twMerge } from "tailwind-merge";
+
+type Props = {
+  children?: React.ReactNode;
+  className?: string;
+  animated?: boolean;
+  animateProps?: { transit: boolean; toggletransit: () => void };
+  delay?: number;
+};
+export default function Container({
+  children,
+  className,
+  animated,
+  animateProps,
+  delay,
+}: Props) {
+  if (animated && animateProps)
+    return (
+      <motion.div
+        animate={
+          animateProps?.transit
+            ? { x: [0, -100], opacity: [1, 0] }
+            : { x: [100, 0], opacity: [0, 1] }
+        }
+        transition={{
+          ease: "easeInOut",
+          duration: 0.6,
+          delay: delay ? delay : 0,
+        }}
+        className={twMerge("max-w-3xl flex flex-col gap-y-8", className)}
+      >
+        {children}
+      </motion.div>
+    );
+  return (
+    <div className={twMerge("max-w-3xl flex flex-col gap-y-8", className)}>
+      {children}
+    </div>
+  );
+}
--- src/components/utils/PrimaryButton.tsx
+++ src/components/utils/PrimaryButton.tsx
@@ -6,24 +6,27 @@ import { motion } from "framer-motion";
 type PrimaryButtonProps = {
   children?: React.ReactNode;
   className?: string;
+  onClick?: () => void;
 };
 
 export default function PrimaryButton({
   children,
   className,
+  onClick,
 }: PrimaryButtonProps) {
   {
     return (
       <motion.button
         whileHover={{
-          opacity: 0.8,
+          opacity: 0.9,
           transition: { duration: 0.3 },
         }}
-        whileTap={{ scale: 0.95, y: 5 }}
+        whileTap={{ scale: 0.97 }}
         className={twMerge(
-          "py-2 px-10 bg-gradient-to-br from-[#be4bdb] to-[#9775fa] text-slate-50 rounded-md shadow-md",
+          "py-2 px-10 bg-gradient-to-br from-[#7950f2] to-[#4dabf7] text-slate-50 rounded-full shadow-lg",
           className
         )}
+        onClick={onClick}
       >
         {children}
       </motion.button>
