2ea77ae15bab1463946faae4953dcdd01af83db2
Ike <ikelimjl@gmail.com>
2023-10-05 21:39:25 +0800

    content component

--- src/components/Instructions.tsx
+++ src/components/Instructions.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { motion } from "framer-motion";
 import PrimaryButton from "./utils/PrimaryButton";
 import useNextTransition from "../hooks/useNextTransition";
@@ -30,12 +30,14 @@ export function Intro() {
           transition={{ ease: "easeInOut", duration: 0.6, delay: 0.05 }}
         >
           {" "}
-          This simple and interactive questionnaire is like your own personality
+          This simple and interactive activity is like your own personality
           mirror, reflecting who you truly are and giving you a glimpse of how
           you stack up against your fantastic team members. Remember, there are
           no <span className="text-clip gradient-text-2">right</span> or{" "}
           <span className="text-clip gradient-text-3">wrong</span> answers here;
-          it's all about discovering your unique self.
+          it's all about discovering your unique self. <br />
+          <br />
+          Simply click on any of the words that resonate with who you are.
         </motion.p>
       </div>
 
@@ -51,20 +53,6 @@ export function Intro() {
           Next
         </PrimaryButton>
       </motion.a>
-    </Container>
-  );
-}
-
-function InstructionsTwo() {
-  const { transit, toggletransit } = useNextTransition({});
-
-  return (
-    <Container animated={true} animateProps={{ transit, toggletransit }}>
-      <h1>
-        Take a peek at these words and pick out the four that scream "you!" Be
-        honest with yourself—this is all about who you are, not who you wish you
-        could be
-      </h1>
     </Container>
   );
 }
