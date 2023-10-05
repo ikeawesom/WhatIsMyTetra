03139805f7d5de0aebbd036425238414aa263f72
Ike <ikelimjl@gmail.com>
2023-10-05 21:39:51 +0800

    data models for database

---
+++ src/dataModels.ts
@@ -0,0 +1,51 @@
+export const ELEMENTS_DATA = {
+    "Earth": {
+        "word":"firm",
+        "desc": "Earthy folks are like the sturdy oaks of the personality forest. They strut with confidence, tackling goals and control like it's their favorite sport. Risky decisions? No sweat. Quick, possibly hair-raising choices are their forte."
+    },
+    "Air": {
+        "word":"clear",
+        "desc": "Airheads? Not a chance. Air personalities are the Sherlock Holmes of the gang. They're all about the brains. Logical solutions are their playground, and they plan things out like it's a top-secret mission. Accuracy and quality? They're the best at it."
+    },
+    "Water": {
+        "word":"calm",
+        "desc": "Ah, the water wizards. These folks are the glue holding families and teams together. They're the emotional rock stars, with loyalty that could put a dog's devotion to shame. Steadfast like a tortoise, patient as a saint, and with a desire for harmony that would make even a zen master jealous."
+    },
+    "Fire": {
+        "word":"bright",
+        "desc": "Firecrackers, anyone? Fire personalities light up life's party. They're all about the sunny side, exploring possibilities with the zeal of a kid in a candy store. They're the rainbow in your storm, the variety seekers, and they've got more fun up their sleeve than a magician at a birthday bash."
+    }
+}
+
+export const WORDS = [
+    {word: "Firm", number: 11},
+    {word: "Support", number: 33},
+    {word: "Inspiration", number: 64},
+    {word: "Easily distracted", number: 24},
+    {word: "Blunt", number: 41},
+    {word: "Systems", number: 52},
+    {word: "Possibilities", number: 34},
+    {word: "Hesitant", number: 23},
+    {word: "Decisive", number: 61},
+    {word: "Critical", number: 22},
+    {word: "Logical", number: 42},
+    {word: "Take things to heart", number: 53},
+    {word: "Inclusive", number: 43},
+    {word: "Results", number: 51},
+    {word: "Party animal", number: 44},
+    {word: "Rational", number: 72},
+    {word: "Bright", number: 14},
+    {word: "Get to the point", number: 21},
+    {word: "Fun", number: 54},
+    {word: "Clear", number: 12},
+    {word: "Patient", number: 73},
+    {word: "Take action", number: 71},
+    {word: "Patient", number: 73},
+    {word: "Orderly", number: 62},
+    {word: "Facts", number: 31},
+    {word: "Calm", number: 13},
+    {word: "Weigh up pros & cons", number: 32},
+    {word: "Optimistic", number: 74},
+    {word: "Feelings", number: 63},
+
+]
