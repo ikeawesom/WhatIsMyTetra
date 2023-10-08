32f19e4a94665487bb4047905acec379f587edb0
Ike <ikelimjl@gmail.com>
2023-10-08 18:58:22 +0800

    added button styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,23 +1,22 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    './src/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}", "./app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "design-1": "linear-gradient(135deg, #7950f2 5%, #f783ac 95%)",
+        "design-2": "linear-gradient(135deg, #7950f2 5%, #4dabf7 95%)",
       },
       fontFamily: {
-        'header':"'Londrina Solid', 'cursive'",
-        "default":"'Onest', 'sans-serif'"
-      }
+        header: "'Londrina Solid', 'cursive'",
+        default: "'Onest', 'sans-serif'",
+      },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
