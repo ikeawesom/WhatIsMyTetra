16686874c70703ea7495b913cd9bda627e211d02
ikeawesom <ikelimjl@gmail.com>
2024-01-02 16:02:43 +0800

    created results screen

---
+++ app/results/page.tsx
@@ -0,0 +1,11 @@
+import ResultsScreen from "@/src/components/ResultsScreen";
+import DefScreen from "@/src/components/utils/DefScreen";
+import React from "react";
+
+export default function ResultsPage() {
+  return (
+    <DefScreen>
+      <ResultsScreen />
+    </DefScreen>
+  );
+}
