0c394ca5ee8715dd180d9529f6888f74a8bf5c09
ikeawesom <ikelimjl@gmail.com>
2024-01-02 16:04:00 +0800

    renamed project

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Elementaire | Unlock your team potential",
+  title: "WhatIsMyTetra | Unlock your team potential",
 };
 
 export default function RootLayout({
