68be78f6b7b32bba5611c5df2c40c8d5117714b7
ikeawesom <ikelimjl@gmail.com>
2024-01-02 10:56:43 +0800

    using local constant

--- app/quiz/page.tsx
+++ app/quiz/page.tsx
@@ -1,17 +1,16 @@
 import React from "react";
 import DefScreen from "@/src/components/utils/DefScreen";
 import Container from "@/src/components/utils/Container";
-
-import WordHandler from "@/src/supabase/wordsHandler";
 import WordScreen from "@/src/components/WordScreen";
+import { WORDS } from "@/src/constants/dataModels";
 
 export default async function Quiz() {
-  const data = await WordHandler.fetchWords();
+
 
   return (
     <DefScreen>
       <Container>
-        <WordScreen words={data} />
+        <WordScreen words={WORDS} />
       </Container>
     </DefScreen>
   );
